02e76afc57046312104df143b4375996ce4cb68d
Subendu Ghosh <subendughosh18@gmail.com>
2026-03-13 11:56:24 +0530

    Change served HTML file to 'time-paisa-manager.html'

--- server.js
+++ server.js
@@ -6,9 +6,10 @@ const PORT = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname)));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'time-paisa-manager (1).html'));
 });
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
+
 });
